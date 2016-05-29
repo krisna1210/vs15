a298dbb221f609d700294d87d153fca3636a9fc3
krisna1210 <krisna1210@gmail.com>
2016-05-29 21:51:34 +0530

    test

---
+++ AngularEx/AngularEx/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AngularEx
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+         
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/AwesomeAngularMVCApp")
+                    .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                    .IncludeDirectory("~/Scripts/Factories", "*.js")
+                    .Include("~/Scripts/AngularMVCApp.js"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ sampleEntity/sampleEntity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sampleEntity.Startup))]
+namespace sampleEntity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
